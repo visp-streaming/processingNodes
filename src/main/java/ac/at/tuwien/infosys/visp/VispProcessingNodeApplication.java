751d6d5badf93a5f626d6d4397dd8061440fc8e0
Christoph Hochreiner <ch.hochreiner@gmail.com>
2015-07-16 14:09:00 +0200

    initial commit

---
+++ src/main/java/ac/at/tuwien/infosys/visp/VispProcessingNodeApplication.java
@@ -0,0 +1,12 @@
+package ac.at.tuwien.infosys.visp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VispProcessingNodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VispProcessingNodeApplication.class, args);
+    }
+}
---
+++ src/test/java/ac/at/tuwien/infosys/visp/VispProcessingNodeApplicationTests.java
@@ -0,0 +1,16 @@
+package ac.at.tuwien.infosys.visp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = VispProcessingNodeApplication.class)
+public class VispProcessingNodeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
