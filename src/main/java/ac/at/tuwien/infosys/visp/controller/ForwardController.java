f8c3e5fa986a9d69bddadda9f12a48b266f5c077
Christoph Hochreiner <ch.hochreiner@gmail.com>
2015-07-21 17:01:34 +0200

    added waiting period for setup

--- src/main/java/ac/at/tuwien/infosys/visp/controller/ForwardController.java
+++ src/main/java/ac/at/tuwien/infosys/visp/controller/ForwardController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ForwardController {
@@ -16,16 +15,13 @@ public class ForwardController {
 
 
     @RequestMapping(value = "/forward", method = RequestMethod.POST)
-    public String forwardMessage(@RequestBody Message message) {
+    public Message forwardMessage(@RequestBody Message message) {
         LOG.info("Received message with id: " + message.getId());
 
-        RestTemplate restTemplate = new RestTemplate();
-
-        //TODO make url for masternode flexible (set it while spawning a new VM)
-        //restTemplate.put("http://localhost:10000/processedResult", message, Message.class);
+        LOG.info("Don nothing with: " + message.getId());
 
         LOG.info("Forwarded message with id: " + message.getId());
-        return "OK";
+        return message;
     }
 
 
