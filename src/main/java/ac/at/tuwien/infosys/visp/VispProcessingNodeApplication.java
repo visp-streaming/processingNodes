f796a4817f40ba8d5c2e4b77a69d78d7557c8ccc
Christoph Hochreiner <ch.hochreiner@gmail.com>
2015-09-19 17:57:29 +0200

    make a war

--- src/main/java/ac/at/tuwien/infosys/visp/VispProcessingNodeApplication.java
+++ src/main/java/ac/at/tuwien/infosys/visp/VispProcessingNodeApplication.java
@@ -2,9 +2,16 @@ package ac.at.tuwien.infosys.visp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class VispProcessingNodeApplication {
+public class VispProcessingNodeApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder appication) {
+        return appication.sources(VispProcessingNodeApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(VispProcessingNodeApplication.class, args);
